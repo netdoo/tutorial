9f0cbb4ea87768f689c30e93576f982e69a455e0
jhkwon78 <jhkwon78@tmon.co.kr>
2017-07-17 14:17:41 +0900

    .

--- java-sample/ExTime/src/main/java/com/extime/App.java
+++ java-sample/ExTime/src/main/java/com/extime/App.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class App {
@@ -33,9 +34,45 @@ public class App {
         logger.info("elapsed seconds {} (secs)", elapsedSecs);
     }
 
+    public static void getTimestamp() {
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")); // 2015-04-18 00:42:24
+        logger.info("now {}", now);
+    }
+
+
+    public static String getMainSep() {
+        return ";";
+    }
+
+    public static String getUserSep() {
+        return "/";
+    }
+
+    public static String getActionSep() {
+        return "●";
+    }
+
+    public static void foo() {
+        StringBuffer log = new StringBuffer();
+        User user = new User();
+        DealSearchWeight dealSearchWeight = new DealSearchWeight();
+
+        log.append(user.getMemberId()).append(getUserSep()).append(user.getUsername())
+                .append(getMainSep())
+                .append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .append(getMainSep())
+                .append("정렬 가중치 추가 (")
+                .append("키워드 : ").append(String.join(";", dealSearchWeight.getKeyword())).append(",")
+                .append("가중치 : ").append(dealSearchWeight.getKeywordWeight()).append(")");
+
+        dealSearchWeight.setUpdateHistory(log.toString());
+    }
+
     public static void main( String[] args ) throws Exception {
         diffSecs();
         diffDays();
+        getTimestamp();
+        foo();
         System.out.println( "Hello World!" );
     }
 }
