8fc18899694294467243c007d527bf1b01cea10f
jhkwon78 <jhkwon78@tmon.co.kr>
2017-08-22 12:50:27 +0900

    .

---
+++ spring-sample/exsimpleweb/src/main/java/com/exsimpleweb/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.exsimpleweb.controller;
+
+import com.exsimpleweb.model.MainParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(MainParam mainParam) {
+        logger.info("Param {}", mainParam);
+        return "Hello World " + mainParam.getName();
+    }
+}
+
+
