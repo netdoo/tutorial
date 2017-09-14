31c71d5e8bdd083e556f32549c68332b517aaed4
jhkwon78 <jhkwon78@tmon.co.kr>
2017-09-14 09:46:57 +0900

    .

--- elastic-sample/esfuncscore/src/main/java/com/esfuncscore/App.java
+++ elastic-sample/esfuncscore/src/main/java/com/esfuncscore/App.java
@@ -59,7 +59,7 @@ public class App {
                         randomFunction(123)
                 )
         };
-aaaaaaabbbb
+aaaaaaabbbb--local mine
         MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
         QueryBuilder queryBuilder = QueryBuilders.functionScoreQuery(matchAllQueryBuilder, functions)
                 .boost(5)
