e3dfc3b2cc27b740d0a97d91afad00f4a7a81969
Jin Ho Kwon <aswip78@gmail.com>
2017-09-14 09:46:37 +0900

    a
    a

--- elastic-sample/esfuncscore/src/main/java/com/esfuncscore/App.java
+++ elastic-sample/esfuncscore/src/main/java/com/esfuncscore/App.java
@@ -59,7 +59,7 @@ public class App {
                         randomFunction(123)
                 )
         };
-aaaaaaabbbb
+aaaaaaabbbb-- local another
         MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
         QueryBuilder queryBuilder = QueryBuilders.functionScoreQuery(matchAllQueryBuilder, functions)
                 .boost(5)
