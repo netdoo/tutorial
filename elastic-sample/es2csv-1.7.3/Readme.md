744fbbd79ec98a081ef48e7f18c5131bf9ab6aae
JinHoKwon <aswip78@gmail.com>
2017-10-17 23:09:58 +0900

    .

---
+++ elastic-sample/es2csv-1.7.3/Readme.md
@@ -0,0 +1,154 @@
+# Elasticsearch 검색 결과를 CSV 로 덤프하기
+
+
+# 샘플 색인
+```
+PUT cafe
+{
+    "mappings": {
+        "menu" : {
+            "properties": {
+                "name" : {
+                    "type": "string",
+                    "index": "analyzed",
+                    "term_vector": "with_positions_offsets"
+                },
+                "price" : {
+                    "type" : "long",
+                    "index" : "not_analyzed"
+                },
+                "comment"  : {
+                    "type" : "nested",
+                        "properties" : {
+                            "taste" : {"type" : "string"},
+                            "season" : {"type" : "string"},
+                            "takeout" : {"type" : "boolean"}
+                        }
+                    }
+            }
+        }
+    }
+}
+
+PUT cafe/menu/1
+{
+    "name" : "mango magic black tea",
+    "price" : 1000,
+    "comment" : {
+        "taste" : "sweet",
+        "season" : "summer",
+        "takeout" : true
+    }
+}
+
+PUT cafe/menu/2
+{
+    "name" : "herb magic light green tea",
+    "price" : 2000,
+    "comment" : {
+        "taste" : "good sweet",
+        "season" : "summer",
+        "takeout" : true
+    }
+}
+
+PUT cafe/menu/3
+{
+    "name" : "water melon magic fresh tea",
+    "price" : 3000,
+    "comment" : {
+        "taste" : "sweet",
+        "season" : "spring",
+        "takeout" : false
+    }
+}
+
+
+PUT cafe/menu/4
+{
+    "name" : "water melon magic fresh tea",
+    "price" : 4000,
+    "comment" : {
+        "taste" : "sweet",
+        "season" : "spring",
+        "takeout" : false
+    }
+}
+```
+
+
+
+# span_or 검색
+
+mango 또는 herb 토큰에 대해서 or 검색
+
+```
+GET cafe/_search
+{
+   "query": {
+      "span_or": {
+         "clauses": [
+            {"span_term": {"name": "mango"}},
+            {"span_term": {"name": "herb"}}
+         ]
+      }
+   }
+}
+```
+
+# span_or 검색결과
+```
+{
+   "took": 34,
+   "timed_out": false,
+   "_shards": {
+      "total": 5,
+      "successful": 5,
+      "failed": 0
+   },
+   "hits": {
+      "total": 2,
+      "max_score": 1.6739764,
+      "hits": [
+         {
+            "_index": "cafe",
+            "_type": "menu",
+            "_id": "1",
+            "_score": 1.6739764,
+            "_source": {
+               "name": "mango magic black tea",
+               "price": 1000,
+               "comment": {
+                  "taste": "sweet",
+                  "season": "summer",
+                  "takeout": true
+               }
+            }
+         },
+         {
+            "_index": "cafe",
+            "_type": "menu",
+            "_id": "2",
+            "_score": 1.6437844,
+            "_source": {
+               "name": "herb magic light green tea",
+               "price": 2000,
+               "comment": {
+                  "taste": "good sweet",
+                  "season": "summer",
+                  "takeout": true
+               }
+            }
+         }
+      ]
+   }
+}
+```
+
+
+
+
+
+
+
+
---
+++ elastic-sample/es2csv-1.7.3/src/main/java/com/es2csv/App.java
@@ -0,0 +1,80 @@
+package com.es2csv;
+
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.management.Query;
+
+import java.io.FileWriter;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+public class App {
+    final static Logger logger = LoggerFactory.getLogger(App.class);
+    static final String INDEX = "cafe";
+    static final String TYPE = "menu";
+    static final String HOST = "127.0.0.1";
+    static final int PORT = 9300;
+    static final String QUERY_JSON_FILE = "C:\\temp\\search.json";
+    static final String CSV_OUT_FILE = "C:\\temp\\out.csv";
+
+    public static void map2csv(Map<String, Object> map, StringBuilder csv) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() instanceof Map)  {
+                map2csv((Map<String, Object>)entry.getValue(), csv);    // nested object 인 경우, 재귀로 csv 덤프
+            } else {
+                csv.append(entry.getValue().toString()).append(",");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try (FileWriter csvWriter = new FileWriter(CSV_OUT_FILE)) {
+
+            Settings settings = ImmutableSettings.settingsBuilder()
+                    .put("cluster.name", "my-elastic").build();
+
+            TransportClient client = new TransportClient(settings)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(HOST), PORT));
+
+            SearchResponse r = client.prepareSearch(INDEX)
+                    .setTypes(TYPE)
+                    .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                    .setQuery(QueryBuilders.wrapperQuery(new String(Files.readAllBytes(Paths.get(QUERY_JSON_FILE)), StandardCharsets.UTF_8)))
+                    .setFrom(0)
+                    .setSize(10000)
+                    .get();
+
+            StringBuilder csv = new StringBuilder();
+
+            for (SearchHit hit : r.getHits()) {
+                csv.setLength(0);
+                map2csv(hit.getSource(), csv);
+                csvWriter.append(csv.replace(csv.length()-1, csv.length(), "\n").toString());
+            }
+
+            csvWriter.flush();
+            csvWriter.close();
+        } catch (Exception e) {
+            logger.error("Exception", e);
+        }
+    }
+}
