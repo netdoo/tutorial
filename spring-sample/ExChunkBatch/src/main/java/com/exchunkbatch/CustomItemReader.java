8a6eff61f2b99e4a3be8d7c09c3804ef148418c6
JinHoKwon <aswip78@gmail.com>
2017-11-07 21:53:37 +0900

    .

--- spring-sample/ExChunkBatch/src/main/java/com/exchunkbatch/CustomItemReader.java
+++ spring-sample/ExChunkBatch/src/main/java/com/exchunkbatch/CustomItemReader.java
@@ -2,11 +2,16 @@ package com.exchunkbatch;
 
 import java.util.List;
 
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 
-public class CustomItemReader implements ItemReader<String> {
+public class CustomItemReader implements ItemReader<String>, StepExecutionListener {
 
     private List<String> alphabetList;
     private int count = 0;
@@ -15,7 +20,7 @@ public class CustomItemReader implements ItemReader<String> {
     public String read() throws Exception, UnexpectedInputException, ParseException {
         if(count < alphabetList.size()){
             return alphabetList.get(count++);
-        }else{
+        } else {
             return null;
         }
     }
@@ -24,6 +29,22 @@ public class CustomItemReader implements ItemReader<String> {
     }
     public void setAlphabetList(List<String> alphabetList) {
         this.alphabetList = alphabetList;
+    }
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 100_000) {
+            sb.append("Hello Batch 2018");
+        }
+
+        stepExecution.getJobExecution().getExecutionContext().putString("message", sb.toString());
+        return null;
     }
 }
 
--- spring-sample/ExChunkBatch/src/main/java/com/exchunkbatch/PrintTasklet.java
+++ spring-sample/ExChunkBatch/src/main/java/com/exchunkbatch/PrintTasklet.java
@@ -2,12 +2,16 @@ package com.exchunkbatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-public class PrintTasklet implements Tasklet {
+public class PrintTasklet implements Tasklet, StepExecutionListener {
     static final Logger logger = LoggerFactory.getLogger(PrintTasklet.class);
     String message;
 
@@ -18,5 +22,15 @@ public class PrintTasklet implements Tasklet {
     public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
         logger.info("ThreadId {}, Message {}", Thread.currentThread().getId(), message);
         return RepeatStatus.FINISHED;
+    }
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        this.message = stepExecution.getJobExecution().getExecutionContext().getString("message");
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        return null;
     }
 }
