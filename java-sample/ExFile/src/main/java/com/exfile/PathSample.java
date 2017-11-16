4e2fd8c4fd2f9366bee492e83de40583d4be4398
jhkwon78 <jhkwon78@tmon.co.kr>
2017-11-16 10:30:17 +0900

    .

---
+++ java-sample/ExFile/src/main/java/com/exfile/PathSample.java
@@ -0,0 +1,23 @@
+package com.exfile;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by jhkwon78 on 2017-11-16.
+ */
+public class PathSample {
+    final static Logger logger = LoggerFactory.getLogger(PathSample.class);
+    public static void main( String[] args ) throws Exception {
+        Path dir = Paths.get("C:\\temp");
+        Path filename = Paths.get("foo.dat");
+
+        String path = Paths.get("C:\\temp", "foo.dat").toString();
+
+        // print  C:\temp\foo.dat
+        logger.info("{}", path);
+    }
+}
