400b5c748d95d593bb96a1f38ad560f1ca8eb141
jhkwon78 <jhkwon78@tmon.co.kr>
2017-11-22 17:16:05 +0900

    .

--- java-sample/ExRE/src/test/java/com/exre/HanTest.java
+++ java-sample/ExRE/src/test/java/com/exre/HanTest.java
@@ -12,6 +12,24 @@ public class HanTest {
 
     final Logger logger = LoggerFactory.getLogger(HanTest.class);
 
+    /*
+    유니코드에서 한글을 표현하는 방법
+
+    유니코드 범위 목록(Mapping of Unit characters)을 살펴보면,
+    한글 표현을 위한 코드 영역 개수는 다른 언어 글자를 위한 코드 영역 개수보다 대체로 많다는 것을 알 수 있다.
+    유니코드에서 한글을 표현하기 위한 코드 영역은 다음과 같다.
+
+
+    표2 유니코드 범위 목록에서의 한글 관련 범위
+
+    이름	                                        처음	    끝	    개수
+    ==============================================  ====       =====    ====
+    한글 자모 (Hangul Jamo)	                        1100	    11FF	256
+    호환용 한글 자모 (Hangul Compatibility Jamo)	3130	    318F	96
+    한글 자모 확장 A (Hangul Jamo Extended A)	    A960	    A97F	32
+    한글 소리 마디 (Hangul Syllables)	            AC00	    D7AF	11184
+    한글 자모 확장 B (Hangul Jamo Extended B)	    D7B0	    D7FF	80
+     */
     static boolean isHan(char c) {
         if (c >= 0x1100 && c <= 0x11FF) {
             // 한글 자모 (Hangul Jamo)
