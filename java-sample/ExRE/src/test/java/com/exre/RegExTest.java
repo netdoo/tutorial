677919c88c089e8917e9c0b4279a4318b9c633af
JinHoKwon <aswip78@gmail.com>
2017-11-22 22:51:21 +0900

    .

--- java-sample/ExRE/src/test/java/com/exre/RegExTest.java
+++ java-sample/ExRE/src/test/java/com/exre/RegExTest.java
@@ -2,9 +2,15 @@ package com.exre;
 
 
 import com.google.re2j.Pattern;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 
 public class RegExTest {
 
@@ -19,5 +25,55 @@ public class RegExTest {
         logger.info("{}", pattern.matches("BBB"));  // true
         logger.info("{}", pattern.matches("BBBB")); // false
         logger.info("{}", pattern.matches("CCC"));  // false
+    }
+
+    void testDefRegEx(String patternText, String searchText) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        java.util.regex.Pattern defPattern = java.util.regex.Pattern.compile(patternText);
+        Matcher matcher = defPattern.matcher(searchText);
+        boolean result = matcher.find();
+        stopWatch.stop();
+        logger.info("java result {} elapsed time {} (ms)", result, stopWatch.getTime(TimeUnit.MILLISECONDS)); // true
+    }
+
+    void testGoogleRegEx(String patternText, String searchText) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Pattern pattern = Pattern.compile(patternText);
+        boolean result = pattern.matches(searchText);
+        stopWatch.stop();
+        logger.info("google result {} elapsed time {} (ms)", result, stopWatch.getTime(TimeUnit.MILLISECONDS)); // true
+    }
+
+    void testSet(Set<String> dic, String searchText) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        boolean result = dic.contains(searchText);
+        stopWatch.stop();
+        logger.info("set result {} elapsed time {} (ms)", result, stopWatch.getTime(TimeUnit.MILLISECONDS)); // true
+    }
+
+    @Test
+    public void testLargeOrPattern() {
+        String searchText = "000099999";
+        StringBuilder stringBuilder = new StringBuilder();
+        Set<String> dic = new HashSet<>();
+
+        for (int i = 0; i < 100_000; i++) {
+            stringBuilder.append("^").append("0000").append(i).append("$|");
+            dic.add("0000"+i);
+        }
+
+        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        String patternText = stringBuilder.toString();
+        //logger.info("patternText {}", patternText);
+
+        testDefRegEx(patternText, searchText);
+        testGoogleRegEx(patternText, searchText);
+        testSet(dic, searchText);
+        testDefRegEx(patternText, searchText);
+        testGoogleRegEx(patternText, searchText);
+        testSet(dic, searchText);
     }
 }
