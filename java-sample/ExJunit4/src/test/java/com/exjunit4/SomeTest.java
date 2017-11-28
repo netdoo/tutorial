274efef0865ab42459c006da267bf8c3b22fb3b0
jhkwon78 <jhkwon78@tmon.co.kr>
2017-11-28 16:23:02 +0900

    .

---
+++ java-sample/ExJunit4/src/test/java/com/exjunit4/SomeTest.java
@@ -0,0 +1,34 @@
+package com.exjunit4;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jhkwon78 on 2017-11-28.
+ */
+public class SomeTest {
+
+    /*
+
+    1. SomeTest 클래스의 모든 테스트를 실행
+        $ mvn test -Dtest=SomeTest
+
+    2. SomeTest 클래스의 fooTest 만 테스트 함.
+        $ mvn test -Dtest=SomeTest#fooTest
+
+    3. SomeTest 클래스의 *Test와 매칭되는 테스트만 테스트 함.
+        $ mvn test -Dtest=SomeTest#*Test
+     */
+    final static Logger logger = LoggerFactory.getLogger(SomeTest.class);
+
+    @Test
+    public void fooTest() {
+        logger.info("fooTest");
+    }
+
+    @Test
+    public void barTest() {
+        logger.info("barTest");
+    }
+}
