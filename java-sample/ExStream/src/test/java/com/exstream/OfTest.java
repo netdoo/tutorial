b3c9f18a213c1b0b70979b4db5902b42c360cb4b
jhkwon78 <jhkwon78@tmon.co.kr>
2017-11-28 11:24:24 +0900

    .

--- java-sample/ExStream/src/test/java/com/exstream/OfTest.java
+++ java-sample/ExStream/src/test/java/com/exstream/OfTest.java
@@ -6,15 +6,19 @@ import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.stream.Stream;
+
 import static org.junit.Assert.assertTrue;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OfTest {
 
-    final Logger LOGGER = LoggerFactory.getLogger(OfTest.class);
+    final Logger logger = LoggerFactory.getLogger(OfTest.class);
 
     @Test
     public void testOf() {
-
+        String text = "foo";
+        String result = Stream.of(text).map(param -> param.toUpperCase()).findAny().get();
+        logger.info("{}", result);
     }
 }
