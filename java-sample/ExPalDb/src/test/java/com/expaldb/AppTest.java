14029e03ed5923b1fff1fc5436a59da81e67aa61
JinHoKwon <aswip78@gmail.com>
2017-12-05 22:16:15 +0900

    .

--- java-sample/ExPalDb/src/test/java/com/expaldb/AppTest.java
+++ java-sample/ExPalDb/src/test/java/com/expaldb/AppTest.java
@@ -1,23 +1,29 @@
 package com.expaldb;
 
+import com.linkedin.paldb.api.PalDB;
+import com.linkedin.paldb.api.StoreWriter;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 
-/**
- * Unit test for simple App.
- */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AppTest {
 
     final static Logger logger = LoggerFactory.getLogger(AppTest.class);
 
     @Test
     public void testApp() throws Exception {
-        Files.copy(Paths.get("C:\\temp\\abcd.txt"), Paths.get("C:\\temp\\abcd.copy.txt"), StandardCopyOption.REPLACE_EXISTING);
+        StoreWriter writer = PalDB.createWriter(new File("./store.paldb"));
+        writer.put("001", "mbc");
+        writer.put("001", "mbc");
+        writer.close();
     }
 }
