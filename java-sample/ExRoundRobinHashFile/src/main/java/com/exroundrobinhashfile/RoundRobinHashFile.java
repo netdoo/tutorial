4e20c235ef2a89332f9da755a701477920dc77b3
JinHoKwon <aswip78@gmail.com>
2017-12-05 23:28:27 +0900

    .

--- java-sample/ExRoundRobinHashFile/src/main/java/com/exroundrobinhashfile/RoundRobinHashFile.java
+++ java-sample/ExRoundRobinHashFile/src/main/java/com/exroundrobinhashfile/RoundRobinHashFile.java
@@ -1,7 +1,12 @@
 package com.exroundrobinhashfile;
 
 import com.google.code.externalsorting.ExternalSort;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.PrintWriter;
@@ -10,17 +15,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class RoundRobinHashFile {
     String roundRobinDir;
     int maxRR;
     boolean isOpen;
     boolean deleteOnExit;
-    List<PrintWriter> printWriters = new ArrayList<>();
+    Map<Integer, Pair<Path, PrintWriter>> printWriters = new HashMap<>();
+
+    final static Logger logger = LoggerFactory.getLogger(RoundRobinHashFile.class);
 
     public void open(String roundRobinDir, int maxRR, boolean deleteOnExit) throws Exception {
         this.maxRR = maxRR;
@@ -33,11 +40,13 @@ public class RoundRobinHashFile {
             dir.mkdir();
         }
 
-        for (int i = 0; i <= maxRR; i++) {
+        for (int i = 0; i < maxRR; i++) {
             Path curr = Paths.get(this.roundRobinDir, String.valueOf(i) + ".dat");
             Files.write(curr, "".getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             PrintWriter printWriter = new PrintWriter(Files.newBufferedWriter(curr, StandardCharsets.UTF_8, StandardOpenOption.APPEND));
-            printWriters.add(printWriter);
+
+            Pair<Path, PrintWriter> pair = new ImmutablePair<>(curr, printWriter);
+            printWriters.put(Integer.valueOf(i), pair);
         }
 
         this.isOpen = true;
@@ -45,14 +54,67 @@ public class RoundRobinHashFile {
 
     public void println(String key, String value) {
         int idx = Math.abs(key.hashCode()) % maxRR;
-        PrintWriter writer = printWriters.get(idx);
+        Pair<Path, PrintWriter> pair = printWriters.get(idx);
+        PrintWriter writer = pair.getValue();
         writer.println(value);
     }
 
-    public void close() {
-        this.printWriters.forEach(printWriter -> {
+    class MakeUnique implements Runnable {
+        Path readPath;
+        public MakeUnique(Path readPath) {
+            this.readPath = readPath;
+        }
+        @Override
+        public void run() {
+            String line;
+            Map<String, String> map = new HashMap<>();
+            logger.debug("process {}", readPath.toFile().getAbsolutePath());
+            try (BufferedReader in = Files.newBufferedReader(readPath, StandardCharsets.UTF_8);) {
+                int pos;
+                String key, value;
+                while ((line = in.readLine()) != null) {
+                    pos = line.indexOf("\t");
+                    key = line.substring(0, pos);
+                    value = line.substring(pos+1);
+                    map.put(key, value);
+                }
+            } catch (Exception e) {
+                logger.error("fail to make unique ", e);
+            }
+
+            String resultPath = readPath.toFile().getAbsolutePath() + ".result";
+
+            try (PrintWriter out = new PrintWriter(Files.newBufferedWriter(Paths.get(resultPath), StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW));) {
+                map.forEach((k, v) -> {
+                    out.println(v);
+                });
+            } catch (Exception e) {
+                logger.error("fail to make result ", e);
+            }
+
+            logger.debug("done {}", readPath.toFile().getAbsolutePath());
+        }
+    }
+
+    public void close() throws Exception {
+        this.printWriters.forEach((idx, pair) -> {
+            PrintWriter printWriter = pair.getValue();
             printWriter.close();
         });
+
+        ExecutorService p = Executors.newFixedThreadPool(10);
+
+        this.printWriters.forEach((idx, pair) -> {
+            Path readPath = pair.getKey();
+            p.execute(new MakeUnique(readPath));
+        });
+
+        /// shutdown() 메소드 호출전까지 추가된 task 를 처리함.
+        p.shutdown();
+
+        /// 추가된 task가 완료될때까지 10분간 기다림.
+        p.awaitTermination(10, TimeUnit.MINUTES);
+
         this.printWriters.clear();
         this.isOpen = false;
 
--- java-sample/ExRoundRobinHashFile/src/test/java/com/exroundrobinhashfile/AppTest.java
+++ java-sample/ExRoundRobinHashFile/src/test/java/com/exroundrobinhashfile/AppTest.java
@@ -1,6 +1,15 @@
 package com.exroundrobinhashfile;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 import static junit.framework.TestCase.assertTrue;
 
@@ -8,9 +17,43 @@ import static junit.framework.TestCase.assertTrue;
  * Unit test for simple App.
  */
 public class AppTest {
+    final static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    static String getNamedKey(String line) {
+        String cols[] = line.split("\t");
+        if (cols.length < 4) {
+            logger.info("bad line {}", line);
+        }
+        return cols[1] + "." + cols[2];
+    }
+
     @Test
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws Exception {
+
+        String line, trimLine ;
+        String readPath = "C:\\temp\\naver_all.txt";
+        int lineCount = 0;
+        RoundRobinHashFile roundRobinHashFile = new RoundRobinHashFile();
+        roundRobinHashFile.open("C:\\temp\\rr", 512, false);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        try (BufferedReader in = Files.newBufferedReader(Paths.get(readPath), StandardCharsets.UTF_8);) {
+            line = in.readLine();
+            while ((line = in.readLine()) != null) {
+                lineCount++;
+                String key = getNamedKey(line);
+                roundRobinHashFile.println(key, key + "\t" + line);
+                if (lineCount % 100_000 == 0) {
+                    logger.info("process {}", lineCount);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        roundRobinHashFile.close();
+        stopWatch.stop();
+        logger.info("total elapsed time {} (secs)", stopWatch.getTime(TimeUnit.SECONDS));
     }
 }
