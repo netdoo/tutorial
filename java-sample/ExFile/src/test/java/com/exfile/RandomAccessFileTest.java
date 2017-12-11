adf9bc72016f3bc5fb4eb179b6cf3ab2ab805d96
jhkwon78 <jhkwon78@tmon.co.kr>
2017-12-11 18:26:19 +0900

    .

---
+++ java-sample/ExFile/src/test/java/com/exfile/RandomAccessFileTest.java
@@ -0,0 +1,39 @@
+package com.exfile;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class RandomAccessFileTest {
+
+    final static Logger logger = LoggerFactory.getLogger(RandomAccessFileTest.class);
+
+    @Test
+    public void testApp() throws Exception {
+        String testPath = "./raf.txt";
+
+        RandomAccessFile raf = new RandomAccessFile(testPath, "rw");
+        raf.seek(raf.length());
+        raf.write("Red".getBytes());
+        raf.close();
+
+        raf = new RandomAccessFile(testPath, "rw");
+        raf.seek(raf.length());
+        raf.write("Green".getBytes());
+        raf.close();
+
+        raf = new RandomAccessFile(testPath, "rw");
+        raf.seek(0);
+        byte readBuffer[] = new byte[32];
+        logger.info("[{}], [{}]", new String(readBuffer, 0, raf.read(readBuffer, 0, 3)), raf.getFilePointer());
+        logger.info("[{}], [{}]", new String(readBuffer, 0, raf.read(readBuffer, 0, 5)), raf.getFilePointer());
+    }
+}
