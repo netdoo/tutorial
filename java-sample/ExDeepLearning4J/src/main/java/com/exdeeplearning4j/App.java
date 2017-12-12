c1f45503b587738685a89c4361dc3f36f417f3b6
JinHoKwon <aswip78@gmail.com>
2017-12-12 23:20:46 +0900

    .

---
+++ java-sample/ExDeepLearning4J/src/main/java/com/exdeeplearning4j/App.java
@@ -0,0 +1,23 @@
+package com.exdeeplearning4j;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+    final static Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+
+    }
+}
+
---
+++ java-sample/ExDeepLearning4J/src/test/java/com/exdeeplearning4j/AppTest.java
@@ -0,0 +1,23 @@
+package com.exdeeplearning4j;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    final static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @Test
+    public void testApp() throws Exception {
+
+    }
+}
