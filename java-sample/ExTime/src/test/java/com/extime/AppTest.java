1a3f97281829062acb1b618b561208ae8af3bd5e
JinHoKwon <aswip78@gmail.com>
2017-12-13 23:33:54 +0900

    .

--- java-sample/ExTime/src/test/java/com/extime/AppTest.java
+++ java-sample/ExTime/src/test/java/com/extime/AppTest.java
@@ -1,38 +1,30 @@
 package com.extime;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+import static junit.framework.TestCase.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void AppTest() {
+        LocalDate oldDate = LocalDate.of(2017, 12, 12);
+        LocalDate newDate = LocalDate.of(2017, 12, 20);
+
+        assertTrue(oldDate.isBefore(newDate));  // 2017/12/12일이 newDate 이전이면 true
+        assertTrue(newDate.isAfter(oldDate));   // 2017/12/20일이 oldDate 이후라면 true
+        assertTrue(newDate.isEqual(LocalDate.of(2017, 12, 20)));    // newDate가 2017/12/20일 이라면 true
+
+        // DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")); // 2010-11-25T12:30
+
+        assertTrue(newDate.isEqual(LocalDate.parse("20171220", DateTimeFormatter.ofPattern("yyyyMMdd"))));
+        assertTrue(newDate.isEqual(LocalDate.parse("2017/12/20", DateTimeFormatter.ofPattern("yyyy/MM/dd"))));
+        assertTrue(newDate.isEqual(LocalDate.parse("2017-12-20")));
     }
 }
+
