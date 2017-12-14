64f12e1a2e0b1f5e75089e17c78d39a5640e0ca0
JinHoKwon <aswip78@gmail.com>
2017-12-14 22:50:50 +0900

    .

---
+++ java-sample/ExTensorFlow/src/test/java/com/extensorflow/AppTest.java
@@ -0,0 +1,42 @@
+package com.extensorflow;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+
+/**
+ * @memo    https://www.tensorflow.org/install/install_java
+ */
+public class AppTest {
+
+    final static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @Test
+    public void testApp() throws Exception {
+        try (Graph g = new Graph()) {
+            final String value = "Hello from " + TensorFlow.version();
+
+            // Construct the computation graph with a single operation, a constant
+            // named "MyConst" with a value "value".
+            try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+                // The Java API doesn't yet include convenience functions for adding operations.
+                g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+            }
+
+            // Execute the "MyConst" operation in a Session.
+            try (Session s = new Session(g);
+                 Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+                System.out.println(new String(output.bytesValue(), "UTF-8"));
+            }
+        }
+    }
+}
