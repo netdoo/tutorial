e39959da9dc1c40cfd1d7160613974b990236cf8
JinHoKwon <aswip78@gmail.com>
2017-12-15 17:34:00 +0900

    .

--- java-sample/ExTensorFlow/src/test/java/com/extensorflow/AppTest.java
+++ java-sample/ExTensorFlow/src/test/java/com/extensorflow/AppTest.java
@@ -15,6 +15,7 @@ import org.tensorflow.TensorFlow;
 
 /**
  * @memo    https://www.tensorflow.org/install/install_java
+ * https://www.nextobe.com/single-post/2017/07/17/Java-%EC%9D%91%EC%9A%A9%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8%EC%97%90%EC%84%9C-tensorflow-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
  */
 public class AppTest {
 
@@ -40,3 +41,4 @@ public class AppTest {
         }
     }
 }
+
