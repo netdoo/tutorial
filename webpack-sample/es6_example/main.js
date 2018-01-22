115891569d8a1c0b82038d2b0a8e6fe3d30690ae
jhkwon78 <jhkwon78@tmon.co.kr>
2018-01-22 15:01:26 +0900

    .

--- webpack-sample/es6_example/main.js
+++ webpack-sample/es6_example/main.js
@@ -2,7 +2,9 @@ import Utils from './Utils.js'
 import styles from './style.css';
 import moment from 'moment';
 import _ from 'underscore';
+import {hello} from './hello.js';
+import {world} from './world.js';
 
-Utils.log('Hello webpack 123')
+Utils.log(hello + ' ' + world +  ' webpack ');
 document.write(moment().format('YYYY/MM/DD hh:mm:ss') + ' >> ' + _.random(0, 100));
 
