409ca244348755499e6d7275a82ae99fbc309930
JinHoKwon <aswip78@gmail.com>
2018-01-24 23:22:09 +0900

    .

--- java-sample/ExChronicleQueue/src/test/java/com/exchroniclequeue/CycleTest.java
+++ java-sample/ExChronicleQueue/src/test/java/com/exchroniclequeue/CycleTest.java
@@ -21,7 +21,7 @@ import static junit.framework.TestCase.assertEquals;
 public class CycleTest {
 
     final static Logger logger = LoggerFactory.getLogger(CycleTest.class);
-    final String path = "cycle-queue";
+    final String path = "roll";
 
     @Test
     public void _0_테스트_준비() throws Exception {
@@ -29,31 +29,30 @@ public class CycleTest {
     }
 
     @Test
-    public void _1_테스트_() {
-
-        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).build()) {
+    public void _1_큐에쓰기() throws Exception {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).epoch(3_000).build()) {
             ExcerptAppender appender = queue.acquireAppender();
             appender.writeText("1");
+            Thread.sleep(1_000);
             appender.writeText("2");
+            Thread.sleep(1_000);
             appender.writeText("3");
+            Thread.sleep(1_000);
             appender.writeText("4");
-            int a = queue.cycle();
-            appender.writeText("a");
-            appender.writeText("b");
-            appender.writeText("c");
-            appender.writeText("d");
         }
     }
 
     @Test
-    public void _2_테스트_SIZE() {
-        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).build()) {
+    public void _2_큐에서_읽기() throws Exception {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).epoch(3_000).build()) {
             ExcerptTailer tailer = queue.createTailer();
 
-            while (tailer.n)
-
             logger.info("{}", tailer.readText());
+            Thread.sleep(1_000);
             logger.info("{}", tailer.readText());
+            Thread.sleep(1_000);
+            logger.info("{}", tailer.readText());
+            Thread.sleep(1_000);
             logger.info("{}", tailer.readText());
         }
     }
@@ -65,16 +64,24 @@ https://github.com/OpenHFT/Chronicle-Queue
 How to change the time that Chronicle Queue rolls
 The time Chronicle Queue rolls, is based on the UTC time, it uses System.currentTimeMillis().
 
-When using daily-rolling, Chronicle Queue will roll at midnight UTC. If you wish to change the time it rolls, you have to change Chronicle Queue’s epoch() time. This time is a milliseconds offset, in other words, if you set the epoch to be epoch(1) then chronicle will roll at 1 millisecond past midnight.
+When using daily-rolling, Chronicle Queue will roll at midnight UTC.
+If you wish to change the time it rolls,
+you have to change Chronicle Queue’s epoch() time.
+
+This time is a milliseconds offset, in other words,
+if you set the epoch to be epoch(1) then chronicle will roll at 1 millisecond past midnight.
 
 Path path = Files.createTempDirectory("rollCycleTest");
 SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).epoch(0).build();
-We do not recommend that you change the epoch() on an existing system, which already has .cq4 files created, using a different epoch() setting.
+We do not recommend that you change the epoch() on an existing system,
+which already has .cq4 files created, using a different epoch() setting.
 
 If you were to set :
 
 .epoch(System.currentTimeMillis()
-This would make the current time the roll time, and the cycle numbers would start from zero.
+
+This would make the current time the roll time,
+and the cycle numbers would start from zero.
 
 How to find the current cq4 Chronicle Queue is writing to
 WireStore wireStore = queue.storeForCycle(queue.cycle(), 0, false);
