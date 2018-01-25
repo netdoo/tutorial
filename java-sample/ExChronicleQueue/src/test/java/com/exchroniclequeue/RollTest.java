c38a259b7b084183cb802548f2405c48a0f0f4a1
JinHoKwon <aswip78@gmail.com>
2018-01-25 14:03:18 +0900

    .

--- java-sample/ExChronicleQueue/src/test/java/com/exchroniclequeue/RollTest.java
+++ java-sample/ExChronicleQueue/src/test/java/com/exchroniclequeue/RollTest.java
@@ -3,6 +3,9 @@ package com.exchroniclequeue;
 
 import net.openhft.chronicle.queue.ExcerptAppender;
 import net.openhft.chronicle.queue.ExcerptTailer;
+import net.openhft.chronicle.queue.RollCycle;
+import net.openhft.chronicle.queue.RollCycles;
+import net.openhft.chronicle.queue.impl.StoreFileListener;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import org.apache.commons.io.FileUtils;
@@ -30,32 +33,72 @@ public class RollTest {
 
     @Test
     public void _1_큐에쓰기() throws Exception {
-        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).epoch(3_000).build()) {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).rollCycle(RollCycles.TEST_SECONDLY).storeFileListener(new StoreFileListener() {
+            @Override
+            public void onReleased(int i, File file) {
+                if (file != null) {
+                    logger.info("release queue file {}", file.getAbsolutePath());
+                    file.delete();
+                }
+            }
+        }).build()) {
+
             ExcerptAppender appender = queue.acquireAppender();
-            appender.writeText("1");
-            Thread.sleep(1_000);
-            appender.writeText("2");
-            Thread.sleep(1_000);
-            appender.writeText("3");
-            Thread.sleep(1_000);
-            appender.writeText("4");
+
+            for (int i = 0; i < 1_000; i++) {
+                appender.writeText(String.valueOf(i));
+                Thread.sleep(1);
+            }
+
+            ExcerptTailer tailer = queue.createTailer();
+            String text = null;
+
+            while ((text = tailer.readText()) != null) {
+                logger.info("{}", text);
+            }
+        }
+    }
+
+
+    @Test
+    public void _2_큐에서_읽기() throws Exception {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).rollCycle(RollCycles.TEST_SECONDLY).build()) {
+            ExcerptTailer tailer = queue.createTailer();
+            String text = null;
+
+            while ((text = tailer.readText()) != null) {
+                logger.info("{}", text);
+            }
+        }
+    }
+
+    /*
+    @Test
+    public void _3_큐에쓰기() throws Exception {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).rollCycle(RollCycles.TEST_SECONDLY).build()) {
+            ExcerptAppender appender = queue.acquireAppender();
+            appender.writeText("10");
+            appender.writeText("20");
+            appender.writeText("30");
+            appender.writeText("40");
         }
     }
 
     @Test
-    public void _2_큐에서_읽기() throws Exception {
-        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).epoch(3_000).build()) {
+    public void _4_큐에서_읽기() throws Exception {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).rollCycle(RollCycles.TEST_SECONDLY).build()) {
             ExcerptTailer tailer = queue.createTailer();
-
             logger.info("{}", tailer.readText());
-            Thread.sleep(1_000);
             logger.info("{}", tailer.readText());
-            Thread.sleep(1_000);
             logger.info("{}", tailer.readText());
-            Thread.sleep(1_000);
+            logger.info("{}", tailer.readText());
+            logger.info("{}", tailer.readText());
+            logger.info("{}", tailer.readText());
+            logger.info("{}", tailer.readText());
             logger.info("{}", tailer.readText());
         }
     }
+    */
 }
 
 /*
