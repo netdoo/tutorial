fe5528bccd34cfa2a46480c27bc1245be2b015e3
jhkwon78 <jhkwon78@tmon.co.kr>
2018-01-30 11:50:09 +0900

    .

--- spring-sample/exmemcacheweb/src/main/java/com/exmemcacheweb/controller/MainController.java
+++ spring-sample/exmemcacheweb/src/main/java/com/exmemcacheweb/controller/MainController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class MainController {
@@ -20,7 +24,18 @@ public class MainController {
     @Autowired
     CacheService cacheService;
 
+    @Autowired
+    JedisPool jedisPool;
+
     final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @PostConstruct
+    public void setDefaultCacheData() {
+        Jedis jedis = jedisPool.getResource();
+        String padding = StringUtils.leftPad("A", 4096, "0");
+        jedis.set("key", padding);
+        jedis.close();
+    }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
@@ -29,10 +44,19 @@ public class MainController {
         return "Hello World " + mainParam.getName();
     }
 
-    @RequestMapping(value = "/bulk", method = RequestMethod.GET)
+    @RequestMapping(value = "/mcbulk", method = RequestMethod.GET)
     @ResponseBody
-    public String bulk(@RequestParam(value="id", required=false, defaultValue="1") Long id) throws Exception {
+    public String mcbulk(@RequestParam(value="id", required=false, defaultValue="1") Long id) throws Exception {
         String s =  cacheService.getSomeData(id);
+        return s;
+    }
+
+    @RequestMapping(value = "/rebulk", method = RequestMethod.GET)
+    @ResponseBody
+    public String rebulk(@RequestParam(value="id", required=false, defaultValue="1") Long id) throws Exception {
+        Jedis jedis = jedisPool.getResource();
+        String s = jedis.get("key");
+        jedis.close();
         return s;
     }
 }
