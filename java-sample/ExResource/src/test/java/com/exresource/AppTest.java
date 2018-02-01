32cb06930230c68e314a88c38f299ca5f28c2ad4
jhkwon78 <jhkwon78@tmon.co.kr>
2018-02-01 12:39:31 +0900

    .

--- java-sample/ExResource/src/test/java/com/exresource/AppTest.java
+++ java-sample/ExResource/src/test/java/com/exresource/AppTest.java
@@ -1,38 +1,54 @@
 package com.exresource;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AppTest {
+
+    String fileName = "log4j.properties";
+    final static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    @BeforeClass
+    public static void 테스트_준비() throws Exception {
+
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void _1_ReadAllLinesTest() throws Exception {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(classloader.getResourceAsStream(fileName)));) {
+
+            String s = bufferedReader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+
+            logger.info("{}", s);
+
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void _2_ReadLinesTest() throws Exception {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(classloader.getResourceAsStream(fileName)));) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                logger.info("{}", line);
+            }
+        } catch (Exception e) {
+            logger.error("", e);
+        }
     }
 }
