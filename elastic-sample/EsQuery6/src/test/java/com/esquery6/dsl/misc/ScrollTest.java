690825b81bece61c4b5b9f5854e9e7d17f370b21
jhkwon78 <jhkwon78@tmon.co.kr>
2018-02-06 16:03:10 +0900

    .

--- elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/misc/ScrollTest.java
+++ elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/misc/ScrollTest.java
@@ -1,7 +1,7 @@
-package com.esquery6;
+package com.esquery6.dsl.misc;
 
+import com.esquery6.BaseTest;
 import com.esquery6.domain.Alphabet;
-import com.esquery6.domain.Market;
 import org.elasticsearch.ResourceAlreadyExistsException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
@@ -14,21 +14,16 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.IndexNotFoundException;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
-import org.elasticsearch.search.sort.SortMode;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
+import java.util.concurrent.TimeUnit;
 
 import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
@@ -117,37 +112,28 @@ public class ScrollTest extends BaseTest {
 
     @Test
     public void _01_SearchScrollTest() throws Exception {
-
-        FieldSortBuilder sortBuilder = SortBuilders.fieldSort("code").order(SortOrder.ASC);
-
         SearchRequestBuilder builder = esClient.prepareSearch(indexName)
                 .setTypes(typeName)
                 .setScroll(new TimeValue(60000))
                 .setQuery(matchAllQuery())
-                .addSort(sortBuilder)
-                .setSize(10);       // max of 10 hits will be returned for each scroll
+                .addSort(SortBuilders.fieldSort("code").order(SortOrder.ASC))
+                .setSize(10);       // 10개씩 반복해서 검색 결과를 조회함. (최대 검색 결과는 10개)
 
         logger.info("Request Query\nGET {}/{}/_search\n{}", indexName, typeName, builder.toString());
-
         SearchResponse sr = builder.execute().actionGet();
 
         String name;
         Integer code;
 
-        // Scroll until no hits are returned
         do {
-
             for (SearchHit hit : sr.getHits().getHits()) {
-                // Handle the hit...
                 Map<String, Object> source = hit.getSourceAsMap();
-
                 name = (String)source.getOrDefault("name", "");
                 code = (Integer)source.getOrDefault("code", 0);
-
                 logger.info("{} : {}", name, code);
             }
 
-            sr = esClient.prepareSearchScroll(sr.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
+            sr = esClient.prepareSearchScroll(sr.getScrollId()).setScroll(new TimeValue(6, TimeUnit.SECONDS)).execute().actionGet();
         } while(sr.getHits().getHits().length != 0); // Zero hits mark the end of the scroll and the while loop.
     }
 }
