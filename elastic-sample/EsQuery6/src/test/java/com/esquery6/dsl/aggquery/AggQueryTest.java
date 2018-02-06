0cee9cd608c969c93ee1678951b8cbd68c8731b3
jhkwon78 <jhkwon78@tmon.co.kr>
2018-02-06 16:46:40 +0900

    .

--- elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/aggquery/AggQueryTest.java
+++ elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/aggquery/AggQueryTest.java
@@ -2,10 +2,23 @@ package com.esquery6.dsl.aggquery;
 
 import com.esquery6.BaseTest;
 import com.esquery6.dsl.termquery.TermQueryTest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.metrics.max.Max;
+import org.elasticsearch.search.aggregations.metrics.min.Min;
+import org.elasticsearch.search.aggregations.metrics.sum.Sum;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 
 public class AggQueryTest extends BaseTest {
     final static Logger logger = LoggerFactory.getLogger(AggQueryTest.class);
@@ -13,11 +26,27 @@ public class AggQueryTest extends BaseTest {
     @BeforeClass
     public static void 테스트_준비() throws Exception {
         printNodes(logger);
-        initSearchTest(logger);
+//        initSearchTest(logger);
     }
 
     @Test
-    public void _01_() throws Exception {
+    public void _01_MultiAggTest() throws Exception {
+        SearchRequestBuilder builder = esClient.prepareSearch(sampleIndexName)
+                .setTypes(marketTypeName)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(matchAllQuery())
+                .addAggregation(AggregationBuilders.sum("sum_of_price").field("price"))
+                .addAggregation(AggregationBuilders.max("max_of_price").field("price"))
+                .addAggregation(AggregationBuilders.min("min_of_price").field("price"));
 
+        logger.info("GET {}/{}/_search \n{}", sampleIndexName, marketTypeName, builder.toString());
+        SearchResponse sr = builder.get();
+        Aggregations aggregation = sr.getAggregations();
+
+        Sum aggSum = aggregation.get("sum_of_price");
+        Max aggMax = aggregation.get("max_of_price");
+        Min aggMin = aggregation.get("min_of_price");
+
+        logger.info("sum {} max {} min {}", aggSum.getValue(), aggMax.getValue(), aggMin.getValue());
     }
 }
---
+++ elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/misc/MultiSearchTest.java
@@ -0,0 +1,72 @@
+package com.esquery6.dsl.misc;
+
+import com.esquery6.BaseTest;
+import com.esquery6.domain.Alphabet;
+import org.elasticsearch.ResourceAlreadyExistsException;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.search.MultiSearchResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.IndexNotFoundException;
+import org.elasticsearch.search.SearchHit;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+
+public class MultiSearchTest extends BaseTest {
+    final static Logger logger = LoggerFactory.getLogger(MultiSearchTest.class);
+
+    @BeforeClass
+    public static void 테스트_준비() throws Exception {
+        printNodes(logger);
+        initSearchTest(logger);
+    }
+
+    @Test
+    public void _01_MultiSearchTest() throws Exception {
+        SearchRequestBuilder builder1 = esClient.prepareSearch()
+                .setIndices(sampleIndexName)
+                .setTypes(marketTypeName)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(matchQuery("name", "nike").minimumShouldMatch("1"));
+
+        SearchRequestBuilder builder2 = esClient.prepareSearch()
+                .setIndices(sampleIndexName)
+                .setTypes(marketTypeName)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(matchQuery("name", "adidas").minimumShouldMatch("1"));
+
+        MultiSearchResponse sr = esClient.prepareMultiSearch()
+                .add(builder1)
+                .add(builder2)
+                .get();
+
+        String name;
+        Integer price;
+
+        for (MultiSearchResponse.Item item : sr.getResponses()) {
+            SearchResponse response = item.getResponse();
+            long searchCount = response.getHits().getTotalHits();
+
+            for (SearchHit hit : response.getHits()) {
+                Map<String, Object> source = hit.getSourceAsMap();
+                name = (String)source.getOrDefault("name", "");
+                price = (Integer)source.getOrDefault("price", 0);
+                logger.info("score : {}, id : {}, name : {}, price : {}", hit.getScore(), hit.getId(), name, price);
+            }
+        }
+    }
+}
