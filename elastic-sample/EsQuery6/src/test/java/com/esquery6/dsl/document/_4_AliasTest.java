136d7e09a0ce05d558e044cec6613af11c9a6cef
jhkwon78 <jhkwon78@tmon.co.kr>
2018-02-06 19:00:57 +0900

    .

--- elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/document/_4_AliasTest.java
+++ elastic-sample/EsQuery6/src/test/java/com/esquery6/dsl/document/_4_AliasTest.java
@@ -4,8 +4,14 @@ import com.esquery6.BaseTest;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesResponse;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.refresh.RefreshResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.cluster.metadata.AliasMetaData;
 import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.IndexNotFoundException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -18,12 +24,46 @@ import java.util.stream.Collectors;
 public class _4_AliasTest extends BaseTest {
     final static Logger logger = LoggerFactory.getLogger(_4_AliasTest.class);
 
+    static String firstIndexName = "first";
+    static String secondIndexName = "second";
+    static String testTypeName = "test";
+
     String sampleAliasName = "my_alias";
 
     @BeforeClass
     public static void 테스트_준비() throws Exception {
         printNodes(logger);
-        initSearchTest(logger);
+        //initSearchTest(logger);
+
+        try {
+            CreateIndexResponse r = esClient.admin().indices().prepareCreate(firstIndexName).execute().actionGet();
+        } catch (IndexNotFoundException e) {}
+
+        try {
+            CreateIndexResponse r = esClient.admin().indices().prepareCreate(secondIndexName).execute().actionGet();
+        } catch (IndexNotFoundException e) {}
+
+        BulkRequestBuilder bulkRequest = esClient.prepareBulk();
+
+        bulkRequest.add(esClient.prepareIndex(firstIndexName, testTypeName, "1").setSource("{\"name\" : \"mbc\"}", XContentType.JSON));
+        bulkRequest.add(esClient.prepareIndex(firstIndexName, testTypeName, "2").setSource("{\"name\" : \"kbs\"}", XContentType.JSON));
+        bulkRequest.add(esClient.prepareIndex(secondIndexName, testTypeName, "1").setSource("{\"name\" : \"ebs\"}", XContentType.JSON));
+        bulkRequest.add(esClient.prepareIndex(secondIndexName, testTypeName, "2").setSource("{\"name\" : \"sbs\"}", XContentType.JSON));
+
+        BulkResponse r = bulkRequest.execute().actionGet(5000);
+
+        if (r.hasFailures()) {
+            logger.error("fail to bulk insert");
+        } else {
+            logger.info("bulk insert !!");
+        }
+
+        esClient.admin().indices().prepareRefresh(firstIndexName, secondIndexName).get();
+    }
+
+    @Test
+    public void dummy() throws Exception {
+
     }
 
     @Test
