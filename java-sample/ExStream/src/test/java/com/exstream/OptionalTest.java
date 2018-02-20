33d4c5981c79e50fee57be1af76c9575c8d7c628
jhkwon78 <jhkwon78@tmon.co.kr>
2018-02-20 14:26:46 +0900

    .

--- java-sample/ExStream/src/test/java/com/exstream/OptionalTest.java
+++ java-sample/ExStream/src/test/java/com/exstream/OptionalTest.java
@@ -1,5 +1,6 @@
 package com.exstream;
 
+import com.exstream.domain.Box;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -13,28 +14,91 @@ import java.util.stream.Stream;
 public class OptionalTest {
 
     final Logger logger = LoggerFactory.getLogger(OptionalTest.class);
+    private Box defaultBox = new Box("white");
 
-    String getNullText() {
+    Box getNullBox() {
         return null;
     }
 
-    String getNotNullText() {
-        return new String("hello");
+    Box getNotNullBox() {
+        return new Box("orange");
     }
 
     @Test
-    public void _01_NullTest() {
-        // OptionalTest:27 - empty
-        logger.info("{}", Optional.ofNullable(getNullText()).map(s -> {
-            return s.toUpperCase();
-        }).orElse("empty"));
+    public void _01_Null_orElseGet_Test() {
+        /*
+        begin _01_Null_orElseGet_Test
+        Box 생성자 호출됨
+        Box {color : 'null'}
+        finish _01_Null_orElseGet_Test
+        */
+        logger.info("begin _01_Null_orElseGet_Test");
+        logger.info("{}", Optional.ofNullable(getNullBox()).map(box -> {
+            box.setColor(box.getColor().toUpperCase());
+            return box;
+        }).orElseGet(Box::new));
+        logger.info("finish _01_Null_orElseGet_Test");
     }
 
     @Test
-    public void _02_NotNullTest() {
-        // OptionalTest:32 - HELLO
-        logger.info("{}", Optional.ofNullable(getNotNullText()).map(s -> {
-            return s.toUpperCase();
-        }).orElse("empty"));
+    public void _02_NotNull_orElseGet_Test() {
+        /*
+        begin _02_NotNull_orElseGet_Test
+        Box {color : 'ORANGE'}
+        finish _02_NotNull_orElseGet_Test
+        */
+        logger.info("begin _02_NotNull_orElseGet_Test");
+        logger.info("{}", Optional.ofNullable(getNotNullBox()).map(box -> {
+            box.setColor(box.getColor().toUpperCase());
+            return box;
+        }).orElseGet(Box::new));
+        logger.info("finish _02_NotNull_orElseGet_Test");
+    }
+
+    @Test
+    public void _03_Null_orElse_Test() {
+        /*
+        begin _03_Null_orElse_Test
+        Box 생성자 호출됨
+        Box {color : 'null'}
+        finish _03_Null_orElse_Test
+        */
+        logger.info("begin _03_Null_orElse_Test");
+        logger.info("{}", Optional.ofNullable(getNullBox()).map(box -> {
+            box.setColor(box.getColor().toUpperCase());
+            return box;
+        }).orElse(new Box()));
+        logger.info("finish _03_Null_orElse_Test");
+    }
+
+    @Test
+    public void _04_NotNull_orElse_Test() {
+        /*
+        begin _04_NotNull_orElse_Test
+        Box 생성자 호출됨
+        Box {color : 'ORANGE'}
+        finish _04_NotNull_orElse_Test
+        */
+        logger.info("begin _04_NotNull_orElse_Test");
+        logger.info("{}", Optional.ofNullable(getNotNullBox()).map(box -> {
+            box.setColor(box.getColor().toUpperCase());
+            return box;
+        }).orElse(new Box()));
+        logger.info("finish _04_NotNull_orElse_Test");
+    }
+
+    @Test
+    public void _05_Null_orElseDefault_Test() {
+        /*
+        begin _05_Null_orElseDefault_Test
+        Box {color : 'white'}
+        finish _05_Null_orElseDefault_Test
+        */
+        logger.info("begin _05_Null_orElseDefault_Test");
+        logger.info("{}", Optional.ofNullable(getNullBox()).map(box -> {
+            box.setColor(box.getColor().toUpperCase());
+            return box;
+        }).orElse(defaultBox));
+        logger.info("finish _05_Null_orElseDefault_Test");
     }
 }
