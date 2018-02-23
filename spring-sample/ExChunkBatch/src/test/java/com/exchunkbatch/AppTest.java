b6778705c222c81f3d3143eec8af3030d299904d
JinHoKwon <aswip78@gmail.com>
2018-02-23 09:02:18 +0900

    .

--- spring-sample/ExChunkBatch/src/test/java/com/exchunkbatch/AppTest.java
+++ spring-sample/ExChunkBatch/src/test/java/com/exchunkbatch/AppTest.java
@@ -1,12 +1,12 @@
 package com.exchunkbatch;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.listener.CompositeItemProcessListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
