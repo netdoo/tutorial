32ffe38f79a973df698c5aa1b02b9fa95069ef53
JinHoKwon <aswip78@gmail.com>
2018-03-15 22:24:28 +0900

    .

--- java-sample/ExMath/src/test/java/com/exmath/LogTest.java
+++ java-sample/ExMath/src/test/java/com/exmath/LogTest.java
@@ -11,6 +11,10 @@ public class LogTest {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
+    double logB(double x, double base) {
+        return Math.log(x) / Math.log(base);
+    }
+
     String getLogWeight(Long qcNow, Long qcPrev) {
         if (qcPrev == 0) {
             qcPrev = 1L;
@@ -20,10 +24,23 @@ public class LogTest {
         return String.format("%.2f" , result);
     }
 
+    String getLogWeight2(Long qcNow, Long qcPrev, Integer base) {
+        if (qcPrev == 0) {
+            qcPrev = 1L;
+        }
+
+        double result = (qcNow * ((logB(qcNow, base) - logB(qcPrev, base))/2));
+        return String.format("%.2f" , result);
+    }
+
     @Test
     public void _0_테스트_준비() throws Exception {
         logger.info("weight {}", getLogWeight(200L, 100L));
         logger.info("weight {}", getLogWeight(200L, 0L));
         logger.info("weight {}", getLogWeight(30L, 0L));
+
+        logger.info("weight2 {}", getLogWeight2(200L, 100L, 10));
+        logger.info("weight2 {}", getLogWeight2(200L, 0L, 10));
+        logger.info("weight2 {}", getLogWeight2(30L, 0L, 10));
     }
 }
