31f3fa6dd387bfda7dc0cc28ffbcb3238f720a3c
JinHoKwon <aswip78@gmail.com>
2018-03-15 15:50:00 +0900

    .

--- spring-sample/ExJSP/src/main/java/com/exjsp/controller/FormController.java
+++ spring-sample/ExJSP/src/main/java/com/exjsp/controller/FormController.java
@@ -4,7 +4,9 @@ import com.exjsp.domain.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,10 +18,35 @@ public class FormController {
     Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping(value = "/save", consumes = "application/x-www-form-urlencoded")
-    public String save(@RequestBody Member member) {
+    @ResponseBody
+    public Member save(@RequestBody Member member) {
         logger.info("member.name {}", member.getName());
-        return "home";
+        return new Member("test", 10);// "home";
     }
+
+    @PostMapping(value = "/save2", consumes = "application/json")
+    @ResponseBody
+    public Member save2(@RequestBody Member member) {
+        logger.info("member.name {}", member.getName());
+        return new Member("test", 10);// "home";
+    }
+
+    @PostMapping(value = "/save3")
+    @ResponseBody
+    public Member save3(@RequestBody Member member) {
+        logger.info("member.name {}", member);
+        return new Member("test", 10);// "home";
+    }
+
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody  Member uploadFileHandler(@RequestParam("name") String name,                            @RequestParam("age") Integer age) {
+        logger.info("333 member.name {}", name);
+        return new Member("aaa", 1);
+    }
+
+
+
 
     @PostMapping(value = "/put", consumes = "application/x-www-form-urlencoded", produces = "application/json")
     @ResponseBody
