9efa60ea240f624377f666930e3d3d48e927021a
jhkwon78 <jhkwon78@tmon.co.kr>
2018-04-09 18:28:10 +0900

    .

--- elastic-sample/esjest/src/main/java/com/esjest/model/AbstractDocument.java
+++ elastic-sample/esjest/src/main/java/com/esjest/model/AbstractDocument.java
@@ -3,9 +3,11 @@ package com.esjest.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NON_PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class AbstractDocument {
 
     public AbstractDocument() {
